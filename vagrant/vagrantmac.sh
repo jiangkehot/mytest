875bf33d724896daf1c3e987c9724a3fee1fe3a8
jiangkehot <jiangkehot@gmail.com>
2018-01-04 17:59:51 +0800

    Update vagrantmac.sh

--- vagrant/vagrantmac.sh
+++ vagrant/vagrantmac.sh
@@ -3,6 +3,11 @@
 #当shell语句错误即停止，防止错误继续执行
 set -e
 
+#Mac下安装gnu-sed
+#brew install gnu-sed --with-default-names
+#echo "export PATH=\"\/usr\/local\/opt\/gnu-sed\/libexec\/gnubin:$PATH\”" >> ~/.zshrc
+#source ~/.zshrc
+
 #创建并进入目录virtualboxbase
 mkdir virtualboxbase && cd virtualboxbase
 
@@ -12,8 +17,11 @@ vagrant init centos/7
 #获取行号
 #sed -n '/centos\/7/=' vagrantfile |sed -n "1"p
 
-#修改初始化后的Vagrantfile文件，添加可以执行外部sh脚本的语句
+#修改初始化后的Vagrantfile文件
+#添加可以执行外部sh脚本的语句
 sed -i "16a\  config.vm.provision \"shell\", path: \"virtualbox_centos_docker.sh\"" Vagrantfile
+#命名host
+sed -i "16a\  config.vm.hostname = \"virtualbox_base\"" Vagrantfile
 
 
 #下载sh脚本
@@ -24,3 +32,9 @@ vagrant up
 
 #vagrant package 打包生成virtualbox
 vagrant package --output CentOS7_docker
+
+#移除虚拟机
+vagrant destroy virtualbox_base
+
+#移除环境
+cd .. && rm -rf virtualboxbase
