af0e361fec5df6543749e77a5af80ce740c97eb9
jiangkehot <jiangkehot@gmail.com>
2018-02-07 04:20:36 +0800

    Create macbookstartup.sh

---
+++ macbookstartup.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+#Appstore: wechat/ Evernote/ MindNode/ QQ/
+#Safari: yinpin.sogou.com /Astrill /VirtualBox /vagrant / #https://www.vagrantup.com/downloads.html
+
+#sh -x
+#install Hmoebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#cask_GUI
+brew update && brew cask install /
+alfred /
+launchrocket /
+google-chrome /
+adobe-photoshop-cs6
+
+#coding tools
+brew cask install /
+macvim /
+pycharm /
+sublime-text /
+sequel-pro /
+filezilla
