bb45c5f7b695634e2f7dfe0aa5b0f4e6a21bc864
jiangkehot <jiangkehot@gmail.com>
2018-07-05 14:41:14 +0800

    改用官方安装方式

--- docker/installDocker.sh
+++ docker/installDocker.sh
@@ -11,26 +11,26 @@ sudo apt update && apt upgrade -y
 
 
 #官方方式：安装docker
-#curl -fsSL https://get.docker.com/ | sh
+curl -fsSL https://get.docker.com/ | sh
 
 
 
-#cloudman的方式
-# 安装包，允许 apt 命令 HTTPS 访问 Docker 源。
-sudo apt-get install \
-apt-transport-https \
-ca-certificates \
-curl \
-software-properties-common
+# #cloudman的方式
+# # 安装包，允许 apt 命令 HTTPS 访问 Docker 源。
+# sudo apt-get install \
+# apt-transport-https \
+# ca-certificates \
+# curl \
+# software-properties-common
 
-# 添加 Docker 官方的 GPG
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# # 添加 Docker 官方的 GPG
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-# 将 Docker 的源添加到 /etc/apt/sources.list
-sudo add-apt-repository \
-"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) \
-stable"
+# # 将 Docker 的源添加到 /etc/apt/sources.list
+# sudo add-apt-repository \
+# "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+# $(lsb_release -cs) \
+# stable"
 
-#安装docker社区版
-sudo apt-get install docker-ce -y
+# #安装docker社区版
+# sudo apt-get install docker-ce -y
