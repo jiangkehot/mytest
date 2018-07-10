cec8cacbb12d014f212212e0bced4b60bb900168
jiangkehot <jiangkehot@gmail.com>
2018-07-10 23:11:47 +0800

    Update startup.sh

--- docker/dockerMachine/Vagrant/startup.sh
+++ docker/dockerMachine/Vagrant/startup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #可以使用以下命令在本地执行此脚本，其中sh -s 'XXXX'为提供参数命令
-#curl -fsSL https://raw.githubusercontent.com/jiangkehot/mytest/master/docker/dockerMachine/Vagrant/Vagrantfile/startup.sh | sh -s 'newDir' -x
+#curl -fsSL https://raw.githubusercontent.com/jiangkehot/mytest/master/docker/dockerMachine/Vagrant/startup.sh | sh -s 'newDir' -x
 
 #当shell语句错误即停止，防止错误继续执行
 set -e
@@ -14,7 +14,7 @@ mkdir dmVagrant$1 && cd dmVagrant$1
 #下载sh脚本
 scripts=( Vagrantfile vagrantup.sh vagrantupnode.sh )
 for i in "${scripts[@]}"; do 
-	sudo wget https://raw.githubusercontent.com/jiangkehot/mytest/master/docker/dockerMachine/Vagrant/${i} \
+	wget https://raw.githubusercontent.com/jiangkehot/mytest/master/docker/dockerMachine/Vagrant/${i} \
 	-P ./
 done
 
