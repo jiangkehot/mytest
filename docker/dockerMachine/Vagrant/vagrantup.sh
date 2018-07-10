637c83670f0690eadb5a5e4fbd60c14d58b77b73
jiangkehot <jiangkehot@gmail.com>
2018-07-10 15:47:57 +0800

    修改搭建本地docker-manchin环境的Vagrant-Shell脚本

--- docker/dockerMachine/Vagrant/vagrantup.sh
+++ docker/dockerMachine/Vagrant/vagrantup.sh
@@ -1,34 +1,32 @@
 #! /bin/bash
 
-#安装docker-machine,并添加docker-machine补全功能的脚本
+#准备docker-machine的manager环境
 
 #当shell语句错误即停止，防止错误继续执行
 set -e
 
 #设置系统时间为CTS时间
-ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+# ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 #替换为阿里云的源：
-cp  /etc/apt/sources.list  /etc/apt/sources.list_bak && sudo sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list && sudo apt update && apt-get update
+# cp  /etc/apt/sources.list  /etc/apt/sources.list_bak && sudo sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list && sudo apt update && apt-get update
+# curl -fsSL https://raw.githubusercontent.com/jiangkehot/mytest/master/apt/apt_repo.sh | sh -x 
 
 #安装docker
-#curl -fsSL https://github.com/jiangkehot/mytest/blob/master/docker/installDocker.sh | sh -x 
+curl -fsSL https://github.com/jiangkehot/mytest/blob/master/docker/installDocker.sh | sh -x 
 
 
 #安装docker-machine
 curl -fsSL https://raw.githubusercontent.com/jiangkehot/mytest/master/docker/dockerMachine/installDockerMachine.sh|sh -x
 
-
-#添加docker-machine补全功能的脚本
-#curl -fsSL https://raw.githubusercontent.com/jiangkehot/mytest/master/docker/dockerMachine/dockerMachineCompletion.sh|sh -x
-
-
 #生成秘钥对
-#root秘钥对
+# sudo cp -r /vagrant/ssh/id_rsa* /root/.ssh/
+sudo ssh-keygen -q -t rsa -b 2048 -f /root/.ssh/manager_rsa_key -N '' -C "root_jiang@manager"
+mkdir -p /vagrant/ssh && sudo cp -r /root/.ssh/manager_rsa_key* /vagrant/ssh/
+sudo curl https://raw.githubusercontent.com/jiangkehot/mytest/master/ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 # sudo ssh-keygen -t rsa -C "jiangkehot@root"
-sudo cp -r /vagrant/.vagrant/.ssh/id_rsa* /root/.ssh/
-#vagrant秘钥对
-# ssh-keygen -t rsa -C "jiangkehot@vagrant"
+# sudo cp -r /home/vagrant/.ssh/id_rsa* /root/.ssh/
+# sudo cat /home/vagrant/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 
 
 
--- docker/dockerMachine/Vagrant/vagrantupnode.sh
+++ docker/dockerMachine/Vagrant/vagrantupnode.sh
@@ -1,21 +1,22 @@
 #! /bin/bash
 
-#安装docker-machine,并添加docker-machine补全功能的脚本
+#准备docker-machine的node环境
 
 #当shell语句错误即停止，防止错误继续执行
 set -e
 
 #设置系统时间为CTS时间
-ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+# ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 #替换为阿里云的源：
-cp  /etc/apt/sources.list  /etc/apt/sources.list_bak && sudo sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
+# cp  /etc/apt/sources.list  /etc/apt/sources.list_bak && sudo sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
 
 #更新&升级
-sudo apt update && apt-get update
+# sudo apt update && apt-get update
 
 #将manager对应公钥在验证表里注册，配置免密登录
-sudo cat /vagrant/.vagrant/.ssh/id_rsa.pub > /root/.ssh/authorized_keys
+sudo cat /vagrant/ssh/id_rsa.pub > /root/.ssh/authorized_keys
+sudo curl https://raw.githubusercontent.com/jiangkehot/mytest/master/ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 
-#docker加速
-curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://a13e0e77.m.daocloud.io
+# #docker加速
+
