9b03dae765577c76fbd67d11a022ac6e4c96556f
jiangkehot <jiangkehot@gmail.com>
2018-07-15 01:42:55 +0800

    Update vagrantupnode.sh

--- docker/dockerMachine/Vagrant/vagrantupnode.sh
+++ docker/dockerMachine/Vagrant/vagrantupnode.sh
@@ -6,13 +6,13 @@
 set -e
 
 #设置系统时间为CTS时间
-# ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 #替换为阿里云的源：
-# cp  /etc/apt/sources.list  /etc/apt/sources.list_bak && sudo sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
+cp  /etc/apt/sources.list  /etc/apt/sources.list_bak && sudo sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
 
 #更新&升级
-# sudo apt update && apt-get update
+sudo apt update && apt-get update
 
 #将manager对应公钥在验证表里注册，配置免密登录
 sudo cat /vagrant/.vagrant/myssh/manager_rsa_key.pub > /root/.ssh/authorized_keys
