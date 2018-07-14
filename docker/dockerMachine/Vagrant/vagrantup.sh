153069bf52d73e72518f9f292c02529a1048d4f7
jiangkehot <jiangkehot@gmail.com>
2018-07-15 01:45:23 +0800

    Update vagrantup.sh

--- docker/dockerMachine/Vagrant/vagrantup.sh
+++ docker/dockerMachine/Vagrant/vagrantup.sh
@@ -6,11 +6,11 @@
 set -e
 
 #设置系统时间为CTS时间
-# ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 #替换为阿里云的源：
 # cp  /etc/apt/sources.list  /etc/apt/sources.list_bak && sudo sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list && sudo apt update && apt-get update
-# curl -fsSL https://raw.githubusercontent.com/jiangkehot/mytest/master/apt/apt_repo.sh | sh -x 
+curl -fsSL https://raw.githubusercontent.com/jiangkehot/mytest/master/apt/apt_repo.sh | sh -x 
 
 #安装docker
 # curl -fsSL https://raw.githubusercontent.com/jiangkehot/mytest/master/docker/installDocker.sh | sh -x 
@@ -21,7 +21,7 @@ curl -fsSL https://raw.githubusercontent.com/jiangkehot/mytest/master/docker/doc
 
 #生成秘钥对
 # sudo cp -r /vagrant/ssh/id_rsa* /root/.ssh/
-sudo ssh-keygen -q -t rsa -b 2048 -f /root/.ssh/manager_rsa_key -N '' -C "root_jiang@manager"
+sudo ssh-keygen -t rsa -b 2048 -f /root/.ssh/manager_rsa_key -N '' -C "root_jiang@manager"
 mkdir -p /vagrant/.vagrant/myssh && sudo cp -r /root/.ssh/manager_rsa_key* /vagrant/.vagrant/myssh/
 sudo curl https://raw.githubusercontent.com/jiangkehot/mytest/master/ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 # sudo ssh-keygen -t rsa -C "jiangkehot@root"
