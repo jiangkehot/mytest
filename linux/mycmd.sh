d175648172fa9b26216b2d9f291dad5a3877bb1e
jiangkehot <jiangkehot@gmail.com>
2018-08-23 09:29:03 +0800

    Update mycmd.sh

--- linux/mycmd.sh
+++ linux/mycmd.sh
@@ -1,1 +1,8 @@
-alias cdl='cd $1 && ls -la $!'
+# 打开目录，并列出所有内容
+alias cdl='cd $1 && ls -la --color=auto $!'
+
+# （默认使用颜色）列出所有内容
+ll='ls -la --color=auto'
+
+#停止并清除所有容器
+alias qcrq='docker stop $(docker ps -aq) && docker rm $(docker ps -aq)'
