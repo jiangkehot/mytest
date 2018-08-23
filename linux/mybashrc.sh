e5fe20bee5721a964753167d597ab47c15367425
jiangkehot <jiangkehot@gmail.com>
2018-08-23 09:58:52 +0800

    Update mybashrc.sh

--- linux/mybashrc.sh
+++ linux/mybashrc.sh
@@ -1,3 +1,14 @@
+#shell命令：curl -fsSL https://raw.githubusercontent.com/jiangkehot/mytest/master/linux/mybashrc.sh -o .mybashrc && sh -x .mybashrc
+
+# 向.bash_profile添加执行文件的命令
+sudo tee ~/.bash_profile <<-'EOF'
+{
+  if [ -f ~/.bashrc ]; then
+	  . ~/.mybashrc
+  fi
+}
+EOF
+
 # 打开目录，并列出所有内容
 alias cdl='cd $1 && ls -la --color=auto $!'
 
