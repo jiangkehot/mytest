c6d0f5cbcbdf7178dd59db876e968c4b419e85d6
jiangkehot <jiangkehot@gmail.com>
2018-08-23 16:41:35 +0800

    Update cdl.sh

--- linux/bash_conf/cdl.sh
+++ linux/bash_conf/cdl.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 # 自建shell命令:cdl = cd && ll  (打开目录，并列出所有内容)
+# curl -fsSL https://raw.githubusercontent.com/jiangkehot/mytest/master/linux/bash_conf/cdl.sh -o /usr/local/sbin/cdl && chmod +x /usr/local/sbin/cdl && source cdl
 
 function cdl(){
 
