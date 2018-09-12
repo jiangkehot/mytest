86b0b5395f3440aafc53e94776a6e2125af633c9
jiangke <jiangkehot@gmail.com>
2018-09-12 13:14:45 +0800

    调整文件结构

--- myvpn/openvpn/install.sh
+++ myvpn/openvpn/install.sh
@@ -4,17 +4,22 @@ set -e
 
 read -p "输入VPN服务器IP:" VPN_SERVER_IP
 
+#定义数据卷名称，并创建数据卷
 OVPN_DATA="ovpnData"
-
 docker volume create --name $OVPN_DATA
+
+#配置服务器端，并生成证书
 docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_genconfig -u udp://$VPN_SERVER_IP
 docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn ovpn_initpki
 
-# Start OpenVPN server process
+
+
+# 启动服务器 Start OpenVPN server process
 docker run -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --restart=always --cap-add=NET_ADMIN kylemanna/openvpn
 
-# Generate a client certificate without a passphrase
+
+# 生成客户端证书  Generate a client certificate without a passphrase
 docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn easyrsa build-client-full CLIENTNAME nopass
 
-# Retrieve the client configuration with embedded certificates
+# 导出客户端证书 Retrieve the client configuration with embedded certificates
 docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_getclient CLIENTNAME > myclient.ovpn
