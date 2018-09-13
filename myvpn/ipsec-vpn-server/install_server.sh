2c5b8ba9635c85d587a77c3cbdb8edf39b4a1ce0
jiangkehot <jiangkehot@gmail.com>
2018-09-13 15:16:54 +0800

    Update install_server.sh

--- myvpn/ipsec-vpn-server/install_server.sh
+++ myvpn/ipsec-vpn-server/install_server.sh
@@ -6,7 +6,7 @@ read -p "输入VPN服务器IP:" VPN_SERVER_IP
 
 read -p "是否需要执行docker-machine create命令？请输入y/n，继续下一步！" x
 
-[ "$x" == "y" ] && docker-machine create -d generic --generic-ip-address="$VPN_SERVER_IP" testvpn
+[ "$x" == "y" ] && docker-machine create -d generic --generic-ip-address="$VPN_SERVER_IP" "aaa$VPN_SERVER_IP"
 
 eval $(docker-machine env testvpn)
 
