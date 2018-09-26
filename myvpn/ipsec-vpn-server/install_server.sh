9d75f1ea0d7af48ecc53f4adf64c021c123876ba
jiangkehot <jiangkehot@gmail.com>
2018-09-26 14:56:29 +0800

    Update install_server.sh

--- myvpn/ipsec-vpn-server/install_server.sh
+++ myvpn/ipsec-vpn-server/install_server.sh
@@ -4,13 +4,13 @@ set -e
 
 read -p "输入VPN服务器IP:" VPN_SERVER_IP
 
-read -p "是否需要执行docker-machine create命令？请输入y/n，继续下一步！" x
-
-[ "$x" == "y" ] && docker-machine create -d generic --generic-ip-address="$VPN_SERVER_IP" "aaa$VPN_SERVER_IP" &&  eval $(docker-machine env "aaa$VPN_SERVER_IP")
-
-[ "$x" == "n" ] && read -p "Please input HOSTNAME:" && eval $(docker-machine env $HOSTNAME)
-
 # 远程查看该模块是否加载成功：ssh 47.90.242.168 'lsmod | grep -i af_key'
 ssh $VPN_SERVER_IP 'sudo modprobe af_key'
 
-docker-compose up -d
+read -p "Please input HOSTNAME:"
+
+read -p "是否需要执行docker-machine create命令,创建HOST）请输入 ‘y/n’ ，继续下一步！" x
+
+[ "$x" == "y" ] && docker-machine create -d generic --generic-ip-address="$VPN_SERVER_IP" "$HOSTNAME" 
+
+echo 'eval $(docker-machine env '"$HOSTNAME) "'docker-compose up -d'
