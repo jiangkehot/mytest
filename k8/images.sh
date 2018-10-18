f32f77c3e4110b8c842adac0024b3992d6482e26
jiangkehot <jiangkehot@gmail.com>
2018-10-18 20:14:37 +0800

    Update images.sh

--- k8/images.sh
+++ k8/images.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 
-K8S_GRCIO_REPOSITORYS=`docker images | awk '{print $1}' | grep 'k8s.gcr.io'`
+K8S_GRCIO_REPOSITORYS=`docker images | awk '{print $1":"$2}' | grep 'k8s.gcr.io'`
 
 #push images
 for OLD_K8S_GRCIO_REPOSITORY in $K8S_GRCIO_REPOSITORYS; do
-	NEW_K8S_GRCIO_REPOSITORY=`$OLD_K8S_GRCIO_REPOSITORY | sed -n 's/k8s.gcr.io/registry.cn-beijing.aliyuncs.com\/univerdream_k8sgcrio/g'p`
-	docker tag $K8S_GRCIO_REPOSITORY $NEW_K8S_GRCIO_REPOSITORY
+	NEW_K8S_GRCIO_REPOSITORY=`echo $OLD_K8S_GRCIO_REPOSITORY | sed -n 's/k8s.gcr.io/registry.cn-beijing.aliyuncs.com\/univerdream_k8sgcrio/g'p`
+	docker tag $OLD_K8S_GRCIO_REPOSITORY $NEW_K8S_GRCIO_REPOSITORY
 	docker push $NEW_K8S_GRCIO_REPOSITORY
+	docker rmi $NEW_K8S_GRCIO_REPOSITORY
 done
 
 #pull images
 for OLD_K8S_GRCIO_REPOSITORY in $K8S_GRCIO_REPOSITORYS; do
 	NEW_K8S_GRCIO_REPOSITORY=`$OLD_K8S_GRCIO_REPOSITORY | sed -n 's/k8s.gcr.io/registry.cn-beijing.aliyuncs.com\/univerdream_k8sgcrio/g'p`
-	echo 'docker pull $NEW_K8S_GRCIO_REPOSITORY'
+	echo "docker pull $NEW_K8S_GRCIO_REPOSITORY"
 done
