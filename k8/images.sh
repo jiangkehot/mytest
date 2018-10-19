32a2af00203aa09437bbc243da4e598dcff26e5a
jiangkehot <jiangkehot@gmail.com>
2018-10-19 22:54:30 +0800

    Update images.sh

--- k8/images.sh
+++ k8/images.sh
@@ -1,17 +1,39 @@
 #! /bin/bash
 
-K8S_GRCIO_REPOSITORYS=`docker images | awk '{print $1":"$2}' | grep 'k8s.gcr.io'`
 
-#push images
-for OLD_K8S_GRCIO_REPOSITORY in $K8S_GRCIO_REPOSITORYS; do
-	NEW_K8S_GRCIO_REPOSITORY=`echo $OLD_K8S_GRCIO_REPOSITORY | sed -n 's/k8s.gcr.io/registry.cn-beijing.aliyuncs.com\/univerdream_k8sgcrio/g'p`
-	docker tag $OLD_K8S_GRCIO_REPOSITORY $NEW_K8S_GRCIO_REPOSITORY
-	docker push $NEW_K8S_GRCIO_REPOSITORY
-	docker rmi $NEW_K8S_GRCIO_REPOSITORY
-done
+k8sRegistry="k8s.gcr.io"
+localRegistry="localhost:5000"
+aliyunRegistry="registry.cn-beijing.aliyuncs.com/univerdream_k8sgcrio"
+# 经典网络
+# registry-internal.cn-beijing.aliyuncs.com/univerdream_k8sgcrio
+# 专有网络
+# registry-vpc.cn-beijing.aliyuncs.com/univerdream_k8sgcrio
+sudo docker login --username=univerdream $aliyunRegistry
+
+pushimages(){
+
+}
+
+function pushimages(){
+	bfRegistry=$1
+	afRegistry=$2
+
+	k8sImages=$(docker images | awk '{print $1":"$2}' | grep "$bfRegistry" | sed "s/$bfRegistry\///g")
+
+	#push images
+	for K8sImageName in $k8sImages; do
+		docker tag $bfRegistry/$K8sImageName $afRegistry/$K8sImageName
+		docker push $afRegistry/$K8sImageName
+		docker rmi $afRegistry/$K8sImageName
+	done
+
+	for imagesName in $k8sImages; do
+		echo "docker pull $afRegistry/$K8sImageName"
+	done
+}
 
 #pull images
-for OLD_K8S_GRCIO_REPOSITORY in $K8S_GRCIO_REPOSITORYS; do
-	NEW_K8S_GRCIO_REPOSITORY=`$OLD_K8S_GRCIO_REPOSITORY | sed -n 's/k8s.gcr.io/registry.cn-beijing.aliyuncs.com\/univerdream_k8sgcrio/g'p`
-	echo "docker pull $NEW_K8S_GRCIO_REPOSITORY"
+for K8sImageName in $k8sImages; do
+		docker tag $bfRegistry/$K8sImageName $afRegistry/$K8sImageName
+		docker rmi $afRegistry/$K8sImageName
 done
