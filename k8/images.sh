b4930d4bd7c593b0f9afce3dc03e3e27337f27e1
jiangkehot <jiangkehot@gmail.com>
2018-10-20 01:24:32 +0800

    Update images.sh

--- k8/images.sh
+++ k8/images.sh
@@ -33,6 +33,13 @@ function pushimages(){
 }
 
 #pull images
+docker pull registry-internal.cn-beijing.aliyuncs.com/univerdream_k8sgcrio/kube-proxy:v1.12.1
+docker pull registry-internal.cn-beijing.aliyuncs.com/univerdream_k8sgcrio/kube-apiserver:v1.12.1
+docker pull registry-internal.cn-beijing.aliyuncs.com/univerdream_k8sgcrio/kube-controller-manager:v1.12.1
+docker pull registry-internal.cn-beijing.aliyuncs.com/univerdream_k8sgcrio/kube-scheduler:v1.12.1
+docker pull registry-internal.cn-beijing.aliyuncs.com/univerdream_k8sgcrio/etcd:3.2.24
+docker pull registry-internal.cn-beijing.aliyuncs.com/univerdream_k8sgcrio/coredns:1.2.2
+docker pull registry-internal.cn-beijing.aliyuncs.com/univerdream_k8sgcrio/pause:3.1
 for K8sImageName in $k8sImages; do
 		docker tag $bfRegistry/$K8sImageName $afRegistry/$K8sImageName
 		docker rmi $bfRegistry/$K8sImageName
