7b805d76b881b6b97e1f33435bcfeb1f92e4f626
jiangkehot <jiangkehot@gmail.com>
2018-10-19 10:48:43 +0800

    Update k8Setup.sh

--- k8/k8Setup.sh
+++ k8/k8Setup.sh
@@ -4,6 +4,8 @@
 set -e
 
 # install kubelet / kubeadm / kubectl
+
+# Ubuntu, Debian or HypriotOS
 apt-get update && apt-get install -y apt-transport-https
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
@@ -11,4 +13,17 @@ deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
 apt-get update && apt-get install -y kubelet kubeadm kubectl
+
+# CentOS, RHEL or Fedora
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+yum install -y kubelet kubeadm kubectl
+
 
