59ebe2f1f77ecc494d660986a18e4a7abb9ab562
jiangkehot <jiangkehot@gmail.com>
2019-02-01 14:10:49 +0800

    Create mount.sh

---
+++ mount.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# vim /etc/rc.d/rc.local
+
+# 判断是否存在目录/data，如果没有，自动创建，然后挂载data目录
+[ -d /data/ ] || mkdir /data && sudo mount -t nfs -o vers=4.0,noresvport 17b7148e89-uho45.cn-beijing.nas.aliyuncs.com:/ /data
+
+# 挂载/root目录
+sudo mount -t nfs -o vers=4.0,noresvport 17b7148e89-uho45.cn-beijing.nas.aliyuncs.com:/root /root
+
+#判断是否存在用户jiangke，如果没有，自动创建，然后挂载jiangke的家目录
+[ `grep 'jiangke' /etc/passwd` ] || useradd -m -g root,wheel,docker jiangke && sudo mount -t nfs -o vers=4.0,noresvport 17b7148e89-uho45.cn-beijing.nas.aliyuncs.com:/home/jiangke /home/jiangke
