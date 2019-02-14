cb0f89b7fef49e1fe61fb91f14681b8e24b31c2d
jiangkehot <jiangkehot@gmail.com>
2019-02-15 00:19:50 +0800

    修改hostname

--- linux/centosSetup.sh
+++ linux/centosSetup.sh
@@ -2,6 +2,9 @@
 
 set -x
 
+# 修改hostname
+echo 'bjadmin' > /etc/hostname 
+
 yum update -y && yum upgrade -y
 
 curl https://raw.githubusercontent.com/jiangkehot/mytest/master/ssh/is_rsa.pub > ~/.ssh/authorized_keys
