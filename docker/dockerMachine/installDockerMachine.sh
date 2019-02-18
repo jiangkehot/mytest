20e51361691afce432acf4e8e3d00c8812d6716f
jiangkehot <jiangkehot@gmail.com>
2019-02-18 10:54:34 +0800

    Update installDockerMachine.sh

--- docker/dockerMachine/installDockerMachine.sh
+++ docker/dockerMachine/installDockerMachine.sh
@@ -8,9 +8,10 @@ set -e
 echo 'docker-machine 教程页面：https://docs.docker.com/machine/install-machine/'
 
 #安装docker-machine
+base=https://github.com/docker/machine/releases/download/v0.16.0
 read -p '请输入相应版本的下载地址，如=https://github.com/docker/machine/releases/download/v0.16.0' base
+echo $base
 curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/usr/local/bin/docker-machine && chmod +x /usr/local/bin/docker-machine
-
 
 #添加docker-machine补全功能的脚本
 #因为ubuntu执行时报错，原因是dash作怪，因此执行时声明是bash,不是写sh
