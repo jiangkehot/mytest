18033490d789ba11289768fe4f04e817039d7077
jiangkehot <jiangkehot@gmail.com>
2019-02-21 14:39:43 +0800

    Update install_server.sh

--- myvpn/ipsec-vpn-server/install_server.sh
+++ myvpn/ipsec-vpn-server/install_server.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 
-# curl -O https://raw.githubusercontent.com/jiangkehot/mytest/master/myvpn/ipsec-vpn-server/install_server.sh \
-&& curl -O https://raw.githubusercontent.com/jiangkehot/mytest/master/myvpn/ipsec-vpn-server/docker-compose.yml \
-&& sh -x install_server.sh \
-; rm -f install_server.sh docker-compose.yml
+# curl -O https://raw.githubusercontent.com/jiangkehot/mytest/master/myvpn/ipsec-vpn-server/install_server.sh && curl -O https://raw.githubusercontent.com/jiangkehot/mytest/master/myvpn/ipsec-vpn-server/docker-compose.yml && sh -x install_server.sh ; rm -f install_server.sh docker-compose.yml
 
 set -e
 
