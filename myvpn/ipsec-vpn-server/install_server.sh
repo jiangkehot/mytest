2f8c537d6fa5515fae39eeb21d850a0a053adaff
jiangkehot <jiangkehot@gmail.com>
2019-02-21 10:28:59 +0800

    添加变量$2$3

--- myvpn/ipsec-vpn-server/install_server.sh
+++ myvpn/ipsec-vpn-server/install_server.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+#预设变量
 VPN_SERVER_IP=$1
 
 read -p "输入VPN服务器IP:" VPN_SERVER_IP
@@ -9,10 +10,13 @@ read -p "输入VPN服务器IP:" VPN_SERVER_IP
 # 远程查看该模块是否加载成功：ssh 47.90.242.168 'lsmod | grep -i af_key'
 ssh $VPN_SERVER_IP 'sudo modprobe af_key'
 
+#预设变量
+VPN_SERVER_HOSTNAME=$2
 read -p "输入VPN服务器HOSTNAME:" VPN_SERVER_HOSTNAME
 
+#预设变量
+x=$3
 read -p "是否需要执行docker-machine create命令,创建HOST）请输入 ‘y/n’ ，继续下一步！" x
-
 [ "$x" == "y" ] && docker-machine create -d generic --generic-ip-address="$VPN_SERVER_IP" "$VPN_SERVER_HOSTNAME" 
 
 echo 'eval $(docker-machine env '"$VPN_SERVER_HOSTNAME)"' && docker-compose up -d'
