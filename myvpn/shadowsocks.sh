1d25f6c2ee8e7863373a7a9df9860cebdc6e382f
jiangkehot <jiangkehot@gmail.com>
2019-04-09 09:36:14 +0800

    Update shadowsocks.sh

--- myvpn/shadowsocks.sh
+++ myvpn/shadowsocks.sh
@@ -2,3 +2,5 @@
 
 docker pull oddrationale/docker-shadowsocks
 docker run -d -p 2022:2022 oddrationale/docker-shadowsocks -s 0.0.0.0 -p 2022 -k test123 -m aes-256-cfb
+
+# alias awssdlaunch='ssh -NT -f -D 1080 root@18.144.29.78 && networksetup -setsocksfirewallproxystate Wi-Fi on'
