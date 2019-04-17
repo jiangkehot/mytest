c176276888a045cec4c43901bf705efb6bf3fd65
jiangkehot <jiangkehot@gmail.com>
2019-04-18 00:18:17 +0800

    update

--- linux/mount.sh
+++ linux/mount.sh
@@ -1,39 +1,24 @@
 #! /bin/bash
-
-# alias mymounts='read -p "输入服务器IP：" serverIP && ssh root@${serverIP} "curl https://raw.githubusercontent.com/jiangkehot/mytest/master/linux/mount.sh | sh -x"'
-
 # curl -fsSL https://raw.githubusercontent.com/jiangkehot/mytest/master/linux/mount.sh | sh -x
-
-# vim /etc/rc.d/rc.local  # 自动挂载推荐使用/etc/fstab，而非/etc/rc.d/rc.local，如果使用/etc/rc.d/rc.local，需要添加执行权限，+x
 
 # 判断是否安装了NFS客户端，如果没有，则自动安装
 rpm -q nfs-utils || sudo yum install -y nfs-utils
 
 # 激活环境变量
 source ./my.env 
-
 nfsid=${mynfsid:-'17b7148e89'}
 vswid=${myvswid:-'xfu82'}
 
 
-# 判断是否存在目录/data，如果没有，自动创建，然后挂载data目录
-[ -d /data/ ] || mkdir /data && sudo mount -t nfs -o vers=4.0,noresvport $nfsid-$vswid.cn-beijing.nas.aliyuncs.com:/ /data
-
-# 挂载/root目录
-sudo mount -t nfs -o vers=4.0,noresvport $nfsid-$vswid.cn-beijing.nas.aliyuncs.com:/root /root
-
-#判断是否存在用户jiangke，如果没有，自动创建，然后挂载jiangke的家目录
-[ `grep 'jiangke' /etc/passwd` ] || useradd -m -G root,wheel jiangke && sudo mount -t nfs -o vers=4.0,noresvport $nfsid-$vswid.cn-beijing.nas.aliyuncs.com:/home/jiangke /home/jiangke
-
-
-# 自动挂载
-
 # 声明字典
 declare -A mountPoint
-mountPoint=([/]="/data" [/root]="/root" [/home/jiangke]="/home/jiangke")
+mountPoint=([/]="/data" [/root]="/root" [/home]="/home")
 
-for name in $(echo ${!mountPoint[*]}); do
-  if ! grep "$name ${mountPoint[$name]}" /etc/fstab > /dev/null; then
-    echo "$nfsid-$vswid.cn-beijing.nas.aliyuncs.com:$name ${mountPoint[$name]} nfs4 vers=4.0,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,_netdev,noresvport 0 0" >> /etc/fstab
+for path in $(echo ${!mountPoint[*]}); do
+  # 判断挂载点是否存在，如果没有则自动创建，然后挂载
+  [ -d ${mountPoint[$path]} ] || mkdir -p ${mountPoint[$path]} && sudo mount -t nfs -o vers=4.0,noresvport $nfsid-$vswid.cn-beijing.nas.aliyuncs.com:$path ${mountPoint[$path]}
+  # 添加自动挂载，自动挂载推荐使用/etc/fstab，而非/etc/rc.d/rc.local，如果使用/etc/rc.d/rc.local，需要添加执行权限，+x
+  if ! grep "$path ${mountPoint[$path]}" /etc/fstab > /dev/null; then
+    echo "$nfsid-$vswid.cn-beijing.nas.aliyuncs.com:$path ${mountPoint[$path]} nfs4 vers=4.0,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,_netdev,noresvport 0 0" >> /etc/fstab
   fi
 done
