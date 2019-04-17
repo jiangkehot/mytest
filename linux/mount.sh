d0312e07839789edd4216a50f073aeb523e57bb0
jiangkehot <jiangkehot@gmail.com>
2019-04-17 17:55:29 +0800

    Update mount.sh

--- linux/mount.sh
+++ linux/mount.sh
@@ -9,23 +9,31 @@
 # 判断是否安装了NFS客户端，如果没有，则自动安装
 rpm -q nfs-utils || sudo yum install -y nfs-utils
 
-read -p "输入fid：" fid
-x=${fid:-'xfu82'}
+# 激活环境变量
+source my.env 
+
+nfsid=${mynfsid:-'17b7148e89'}
+vswid=${myvswid:-'xfu82'}
+
 
 # 判断是否存在目录/data，如果没有，自动创建，然后挂载data目录
-[ -d /data/ ] || mkdir /data && sudo mount -t nfs -o vers=4.0,noresvport 17b7148e89-$x.cn-beijing.nas.aliyuncs.com:/ /data
+[ -d /data/ ] || mkdir /data && sudo mount -t nfs -o vers=4.0,noresvport $nfsid-$vswid.cn-beijing.nas.aliyuncs.com:/ /data
 
 # 挂载/root目录
-sudo mount -t nfs -o vers=4.0,noresvport 17b7148e89-$x.cn-beijing.nas.aliyuncs.com:/root /root
+sudo mount -t nfs -o vers=4.0,noresvport $nfsid-$vswid.cn-beijing.nas.aliyuncs.com:/root /root
 
 #判断是否存在用户jiangke，如果没有，自动创建，然后挂载jiangke的家目录
-[ `grep 'jiangke' /etc/passwd` ] || useradd -m -G root,wheel jiangke && sudo mount -t nfs -o vers=4.0,noresvport 17b7148e89-$x.cn-beijing.nas.aliyuncs.com:/home/jiangke /home/jiangke
+[ `grep 'jiangke' /etc/passwd` ] || useradd -m -G root,wheel jiangke && sudo mount -t nfs -o vers=4.0,noresvport $nfsid-$vswid.cn-beijing.nas.aliyuncs.com:/home/jiangke /home/jiangke
 
 
 # 自动挂载
 
-cat >> /etc/fstab <<EOF
-17b7148e89-$x.cn-beijing.nas.aliyuncs.com:/ /data nfs4 vers=4.0,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,_netdev,noresvport 0 0
-17b7148e89-$x.cn-beijing.nas.aliyuncs.com:/root /root nfs4 vers=4.0,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,_netdev,noresvport 0 0
-17b7148e89-$x.cn-beijing.nas.aliyuncs.com:/home/jiangke /home/jiangke nfs4 vers=4.0,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,_netdev,noresvport 0 0
-EOF
+# 声明字典
+declare -A mountPoint
+mountPoint=([/]="/data" [/root]="/root" [/home/jiangke]="/home/jiangke")
+
+for name in $(echo ${!mountPoint[*]}); do
+  [ grep "$name ${mountPoint[$name]}" /etc/fstab ] || cat >> /etc/fstab <<EOF
+  $nfsid-$vswid.cn-beijing.nas.aliyuncs.com:$name ${mountPoint[$name]} nfs4 vers=4.0,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,_netdev,noresvport 0 0
+  EOF
+done
