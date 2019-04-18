7e1443034854fc8f7adbe4e7cb19807cef750e06
jiangkehot <jiangkehot@gmail.com>
2019-04-18 19:30:07 +0800

    修改为禁止密码登录

--- ssh/setup_sshd_config.sh
+++ ssh/setup_sshd_config.sh
@@ -2,10 +2,10 @@
 
 #SSH免密登录
 
-####### 服务器端 #######
-
 #当shell语句错误即停止，防止错误继续执行
 set -e
+
+####### 服务器端 #######
 
 #设置root密码
 #sudo -i
@@ -16,13 +16,13 @@ set -e
 	#no:不允许登录；
 	#prohibit-password：允许秘钥，但禁止密码登录
 #允许root登录，即PermitRootLogin yes
-sed -i 's/PermitRootLogin\ prohibit-password/PermitRootLogin\ yes/g' /etc/ssh/sshd_config
+#sed -i 's/PermitRootLogin\ prohibit-password/PermitRootLogin\ yes/g' /etc/ssh/sshd_config
 
 #允许密码登录
-sed -i 's/PasswordAuthentication\ no/PasswordAuthentication\ yes/g' /etc/ssh/sshd_config
+#sed -i 's/PasswordAuthentication\ no/PasswordAuthentication\ yes/g' /etc/ssh/sshd_config
 
 #重启sshd服务
-systemctl restart sshd.service
+#systemctl restart sshd.service
 
 
 
@@ -38,8 +38,8 @@ systemctl restart sshd.service
 #改回root仅支持秘钥登录设置
 
 #sed -i 's/PermitRootLogin\ yes/PermitRootLogin\ prohibit-password/g' /etc/ssh/sshd_config
-#sed -i 's/PasswordAuthentication\ yes/PasswordAuthentication\ no/g' /etc/ssh/sshd_config
-#systemctl restart sshd.service
+sed -i 's/PasswordAuthentication\ yes/PasswordAuthentication\ no/g' /etc/ssh/sshd_config
+systemctl restart sshd.service
 
 
 
