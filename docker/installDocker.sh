c65a0566bd6d8b485b83d365605b6e0826d51ed2
bjadmin <bjadmin@univerdream.cn>
2019-04-26 21:29:28 +0800

    Update

--- docker/installDocker.sh
+++ docker/installDocker.sh
@@ -11,11 +11,8 @@ set -e
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 
 # 添加docker源
-if [ $1 != 'aliyun' ]; then
-	sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-else
+if [ $1 = 'aliyun' ]; then
 	sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-
 	# docker加速 aliyun加速器
 	sudo mkdir -p /etc/docker
 	#在<<符号后面加一个长划线：cat <<-EOF【注意：<<后面直接跟-否则如果有空格，也会报错~】。这样就可以使用tab来缩进第十行中用来结束输入的终止符(注:  使用空格进行缩进将同样方式报错)
@@ -25,11 +22,13 @@ else
 	}
 	EOF
 	#sudo systemctl daemon-reload && sudo systemctl restart docker
+else
+	sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 fi
 
 #更新并安装相应版本
 sudo yum makecache fast
-[ -z $2 ] && sudo yum -y install docker-ce || sudo yum -y install docker-ce-$1
+[ -z $2 ] && sudo yum -y install docker-ce || sudo yum -y install docker-ce-$2
 #设置开机启动，并启动docker
 sudo systemctl enable docker && sudo systemctl start docker
 
