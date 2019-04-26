4e4f9a0d0a1eeab4381ea8c87d2c1cb0d2fb5164
jiangkehot <jiangkehot@gmail.com>
2019-04-26 14:43:30 +0800

    EOF里的内容需要缩进

--- docker/installDocker.sh
+++ docker/installDocker.sh
@@ -19,9 +19,9 @@ else
     # docker加速 aliyun加速器
     sudo mkdir -p /etc/docker
     sudo tee /etc/docker/daemon.json <<-'EOF'
-    {
-      "registry-mirrors": ["https://0po41ixf.mirror.aliyuncs.com"]
-    }
+        {
+          "registry-mirrors": ["https://0po41ixf.mirror.aliyuncs.com"]
+        }
     EOF
     #sudo systemctl daemon-reload && sudo systemctl restart docker
 fi
