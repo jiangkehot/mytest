c935d656ba877ff380a21cc1986f415495021388
jiangkehot <jiangkehot@gmail.com>
2019-05-15 18:32:33 +0800

    Update cdl.sh

--- linux/bash_conf/cdl.sh
+++ linux/bash_conf/cdl.sh
@@ -6,21 +6,19 @@ function cdl(){
 
 	if [ -z "$1" ]; then
 	  	dir=$HOME
-  	elif [ -z `dirname "$1" | grep '^/'` ]; then  # bug: grep不能操作目录，这里$1是目录
-  		if [[ "$PWD" == '/' ]]; then  # bug：字符串比较不能用数学比较符-eq，需要用==
-  			dir="/$1"
-  		else
-  			dir="$PWD/$1"
-  		fi
-  	else
-		dir=$1
-	fi  
+  	elif echo "$1" | grep -q '^/'; then  # bug: grep不能操作目录，这里$1是目录
+  		dir=$1
+	else
+		dir="$PWD/$1"
+	fi
 
 
 	if [ -d "$dir" ]; then
 		cd "$dir" && ls -lah "$dir"
-	else
+	elif [ -e "$dir" ]; then
 		echo "$dir不是目录"
+	else
+		echo "当前路径下不存在$dir"
 	fi  	
 }
 
