0a4063610a3f5af052b5cdaa40a46962f69d4189
jiangkehot <jiangkehot@gmail.com>
2019-05-15 19:14:02 +0800

    Update cdl.sh

--- linux/bash_conf/cdl.sh
+++ linux/bash_conf/cdl.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # 自建shell命令:cdl = cd && ll  (打开目录，并列出所有内容)
-# curl -fsSL https://raw.githubusercontent.com/jiangkehot/mytest/master/linux/bash_conf/cdl.sh -o .shellib/cdl
+# curl -fsSL https://raw.githubusercontent.com/jiangkehot/mytest/master/linux/bash_conf/cdl.sh -o ~/.shellib/cdl
 
 function cdl(){
 
@@ -22,5 +22,5 @@ function cdl(){
 	fi  	
 }
 
-# 不作为命令，仅作为库文件，被主shell载入：source .shellib/cdl
+# 不作为命令，仅作为库文件，被主shell载入：source ~/.shellib/cdl
 # cdl "$*"
