0dcfcb3f93a24a6164a1de09622ae8d787ff38cc
jiangkehot <jiangkehot@gmail.com>
2019-05-29 00:23:41 +0800

    Update k8Setup.sh

--- k8/k8Setup.sh
+++ k8/k8Setup.sh
@@ -2,43 +2,47 @@
 # curl -fsSL https://raw.githubusercontent.com/jiangkehot/mytest/master/k8/k8Setup.sh | sh -s aliyun -x
 
 setup_k8s(){
-set -e
+set -x
 
 # CentOS, RHEL or Fedora
-
-# google
-cat <<EOF >> /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-exclude=kube*
-EOF
-
-# Aliyun 
-cat <<EOF >> /etc/yum.repos.d/kubernetes.repo
-[kubernetes_aliyun]
-name=Kubernetes
-baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
-EOF
 
 # Set SELinux in permissive mode (effectively disabling it)
 setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
 if [ "$1" == "aliyun"  ]; then
-  yum install -y kubelet kubeadm kubectl --enablerepo=kubernetes_aliyun --disablerepo=kubernetes
+  # Aliyun 
+  if ! grep 'baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/' /etc/yum.repos.d/kubernetes.repo; then
+  cat <<EOF >> /etc/yum.repos.d/kubernetes.repo
+  [kubernetes_aliyun]
+  name=Kubernetes
+  baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
+  enabled=1
+  gpgcheck=1
+  repo_gpgcheck=1
+  gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+  EOF
+  fi
+  
+#   yum install -y kubelet kubeadm kubectl --enablerepo=kubernetes_aliyun --disablerepo=kubernetes
 else
-  yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+  # google
+  if ! grep 'baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64' /etc/yum.repos.d/kubernetes.repo; then
+  cat <<EOF >> /etc/yum.repos.d/kubernetes.repo
+  [kubernetes]
+  name=Kubernetes
+  baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+  enabled=1
+  gpgcheck=1
+  repo_gpgcheck=1
+  gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+  exclude=kube*
+  EOF
+  fi
+#   yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 fi
 
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 systemctl enable kubelet && systemctl start kubelet  
 
 # Enabling shell autocompletion 脚本补全
