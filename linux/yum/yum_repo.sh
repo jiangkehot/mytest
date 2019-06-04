3bec8a4ef032a3ac758dca414ed63ab397ffe753
jiangkehot <jiangkehot@gmail.com>
2019-06-05 01:00:06 +0800

    添加163镜像源

--- linux/yum/yum_repo.sh
+++ linux/yum/yum_repo.sh
@@ -5,11 +5,13 @@
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
 
 #下载阿里云yum源
-
-curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+# curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 #或者
 #wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 
-#生成缓存
-yum makecache
+# 163镜像源
+curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.163.com/.help/CentOS7-Base-163.repo
 
+#生成缓存
+yum clean all
+yum makecache
